ba918a94fd14581d449bc4dae81e7c7b8bdd736f
HarshMan <harshwardhanmandhare04@gmail.com>
2022-01-25 17:48:50 +0530

    Added fn Headers

--- math.py
+++ math.py
@@ -1,8 +1,15 @@
+#ADD IMPLEMENTED
 def add(x,y):
     pass
+
+#SUBSTRACT IMPLEMENTED
 def subtract(x,y):
     pass
+
+#MULTIPLY IMPLEMENTED
 def multiply(x,y):
     pass
+
+#DIVIDE IMPLEMENTED
 def divide(x,y):
     pass
